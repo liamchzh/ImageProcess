679b8f28b36e5167e573c2da64cca781bea695ff
liamchzh <liamchzh@gmail.com>
2012-12-12 15:51:28 +0800

    2012.12.12

--- show_image.py
+++ show_image.py
@@ -304,6 +304,122 @@ def ruihua():
     imgfilted = im.filter(ImageFilter.SHARPEN);
     show_image(imgfilted)
     hist_show(imgfilted)
+
+#基于拉普拉斯算子的边缘检测
+def bianyuan():
+    w,h = im.size
+    nim = Image.new('L',im.size)
+    lim = im.convert('L')
+    lpix = lim.load()
+    npix = nim.load()
+    muban = [0, -1, 0, -1, 4, -1, 0, -1, 0] #拉普拉斯算子模板
+    X = [0 for i in range(9)]
+    for i in range(1,h-1):
+        for j in range(1,w-1):
+            for k in range(3):
+                for l in range(3):
+                    X[k*3+l] = lpix[i-1+k,j-1+l] * muban[k*3+l]
+            npix[i, j] = sum(X)
+    show_image(nim)
+    hist_show(nim)
+
+def canny():
+    Img1 = cv.LoadImage(name,0)
+    PCannyImg = cv.CreateImage(cv.GetSize(Img1), cv.IPL_DEPTH_8U, 1)
+    cv.Canny(Img1, PCannyImg, 50, 150, 3)
+    show_image(ImageTk.PhotoImage(PCannyImg))
+    cv.NamedWindow("canny", 1)
+    cv.ShowImage("canny", PCannyImg)
+    cv.WaitKey(0)
+    cv.DestroyWindow("canny")
+    
+def xihua():
+
+    def VThin(image,array):
+        h = image.height
+        w = image.width
+        NEXT = 1
+        for i in range(h):
+            for j in range(w):
+                if NEXT == 0:
+                    NEXT = 1
+                else:
+                    M = image[i,j-1]+image[i,j]+image[i,j+1] if 0<j<w-1 else 1
+                    if image[i,j] == 0  and M != 0:                  
+                        a = [0]*9
+                        for k in range(3):
+                            for l in range(3):
+                                if -1<(i-1+k)<h and -1<(j-1+l)<w and image[i-1+k,j-1+l]==255:
+                                    a[k*3+l] = 1
+                        sum = a[0]*1+a[1]*2+a[2]*4+a[3]*8+a[5]*16+a[6]*32+a[7]*64+a[8]*128
+                        image[i,j] = array[sum]*255
+                        if array[sum] == 1:
+                            NEXT = 0
+        return image
+
+    def HThin(image,array):
+        h = image.height
+        w = image.width
+        NEXT = 1
+        for j in range(w):
+            for i in range(h):
+                if NEXT == 0:
+                    NEXT = 1
+                else:
+                    M = image[i-1,j]+image[i,j]+image[i+1,j] if 0<i<h-1 else 1   
+                    if image[i,j] == 0 and M != 0:                  
+                        a = [0]*9
+                        for k in range(3):
+                            for l in range(3):
+                                if -1<(i-1+k)<h and -1<(j-1+l)<w and image[i-1+k,j-1+l]==255:
+                                    a[k*3+l] = 1
+                        sum = a[0]*1+a[1]*2+a[2]*4+a[3]*8+a[5]*16+a[6]*32+a[7]*64+a[8]*128
+                        image[i,j] = array[sum]*255
+                        if array[sum] == 1:
+                            NEXT = 0
+        return image
+
+    def Xihua(image,array,num=10):
+        iXihua = cv.CreateImage(cv.GetSize(image),8,1)
+        cv.Copy(image,iXihua)
+        for i in range(num):
+            VThin(iXihua,array)
+            HThin(iXihua,array)
+        return iXihua
+
+    def Two(image):
+        w = image.width
+        h = image.height
+        size = (w,h)
+        iTwo = cv.CreateImage(size,8,1)
+        for i in range(h):
+            for j in range(w):
+                iTwo[i,j] = 0 if image[i,j] < 200 else 255
+        return iTwo
+
+    
+    array = [0,0,1,1,0,0,1,1,1,1,0,1,1,1,0,1,\
+    1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,1,\
+             0,0,1,1,0,0,1,1,1,1,0,1,1,1,0,1,\
+    1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,1,\
+    1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,\
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,\
+    1,1,0,0,1,1,0,0,1,1,0,1,1,1,0,1,\
+             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,\
+             0,0,1,1,0,0,1,1,1,1,0,1,1,1,0,1,\
+    1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,1,\
+             0,0,1,1,0,0,1,1,1,1,0,1,1,1,0,1,\
+    1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,\
+    1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,\
+    1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,\
+    1,1,0,0,1,1,0,0,1,1,0,1,1,1,0,0,\
+    1,1,0,0,1,1,1,0,1,1,0,0,1,0,0,0]
+
+    image = cv.LoadImage(name,0)
+    iTwo = Two(image)
+    iThin = Xihua(iTwo,array)
+    cv.ShowImage('iThin',iThin)
+    cv.WaitKey(0)
     
 def main():
     root = Tk()
@@ -376,6 +492,11 @@ def main():
     menu4.add_command(label = '锐化', command = lambda:ruihua())
     menubar.add_cascade(label = '图像增强', menu = menu4)
 
+    menu5 = Menu(menubar, tearoff = 0)
+    menu5.add_command(label = '边缘检测(La)', command = lambda:bianyuan())
+    menu5.add_command(label = '边缘检测(Canny)', command = lambda:canny())
+    menu5.add_command(label = '细化', command = lambda:xihua())
+    menubar.add_cascade(label = '图像分割', menu = menu5)
     root.config(menu = menubar) 
 
 
